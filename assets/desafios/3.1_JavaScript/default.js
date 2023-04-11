6409868c8926cee8c3196a8b06eb5e4a8fa08122
Juan Antonio <ja.rauldidiaquez@gmail.com>
2023-04-11 09:25:56 -0400

    Estructura base (solo HTML)

--- assets/desafios/3.1_JavaScript/default.js
+++ assets/desafios/3.1_JavaScript/default.js
@@ -1,9 +1,9 @@
 subtitulo = document.querySelector("h2")
 subtitulo.innerHTML = "A ver si logramos viajar por el mundo ahora"
 
-direccionInput = prompt('Ingresa tu dirección')
-departamentoInput = prompt('Ingresa el número de tu departamento')
-comunaInput = prompt('Ingresa tu comuna')
+let direccionInput = prompt('Ingresa tu dirección')
+let departamentoInput = prompt('Ingresa el número de tu departamento')
+let comunaInput = prompt('Ingresa tu comuna')
 
 direccion = document.querySelector(".direccion")
 departamento = document.querySelector(".departamento")
@@ -12,3 +12,8 @@ comuna = document.querySelector(".comuna")
 direccion.innerHTML = direccionInput;
 departamento.innerHTML = departamentoInput;
 comuna.innerHTML = comunaInput;
+
+
+let radiusInput = prompt('¿qué tan redondo quieres la imagen?');
+imagen = document.querySelector('img');
+imagen.style.borderRadius = radiusInput;
